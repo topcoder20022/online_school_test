a59316a33fc594b8989ba37b2d6cbaf65c9c6176
Top Coder <leeyamkeng120@gmail.com>
2021-09-23 08:06:40 +0300

    new data added 2

--- daily_git.py
+++ daily_git.py
@@ -3,25 +3,18 @@ import time
 import subprocess
 import random
 
-date2 = 100
-date1 = 30
+a = 'git status'
+b = 'git add .'
+d = 'git push origin main'
+c = 'git commit -m "fixed"'
+numbers = random.randint(2, 8)
+for j in range(1, numbers):
 
-numbers = 3
-while True:
-	a = 'git status'
-	b = 'git add .'
-	d = 'git push origin main'
-	numbers = random.randint(2, 8)
-	for j in range(1, numbers):
-
-		file_object = open('data.txt', 'a')
-		file_object.write(str(i) + str(j))
-		file_object.close()
-
-		c = 'git commit --date="' + str(i) + ' day ago" -m "new data added ' + str(j) + '"'
-		print(c)
-		e= a + ' && ' + b + ' && ' + c + ' && ' + d 
-		subprocess.call(e, shell = True)
+	file_object = open('data.txt', 'a')
+	file_object.write(str(i) + str(j))
+	file_object.close()
+	e= a + ' && ' + b + ' && ' + c + ' && ' + d 
+	subprocess.call(e, shell = True)
 	
 
 		
