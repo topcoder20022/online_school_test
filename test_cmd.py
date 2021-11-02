c699a79ef6e24a7b3171af36b76c9251a0c204f1
Top Coder <leeyamkeng120@gmail.com>
2021-11-02 07:43:29 +0300

    new data added 1

--- test_cmd.py
+++ test_cmd.py
@@ -2,13 +2,25 @@ import os
 import time
 import subprocess
 
+date1 = 38
+date2 = 40
+numbers = 3
 # while True:
 # os.system("start \"\" cmd /k \"cd /D C:\\ & color 04\"")
 a = 'git status'
 b = 'git add .'
-c = 'git commit -m "fixed"'
 d = 'git push origin main'
-e= a + ' && ' + b + ' && ' + c + ' && ' + d 
-print(e)
-subprocess.call(e, shell = True)
-# subprocess.call(p, shell = True)
+
+for i in range(date1, date2):
+	for j in range(1, numbers):
+
+		file_object = open('data.txt', 'a')
+		file_object.write(str(i) + str(j))
+		file_object.close()
+
+		c = 'git commit --date="' + str(i) + ' day ago" -m "new data added ' + str(j) + '"'
+		print(c)
+		e= a + ' && ' + b + ' && ' + c + ' && ' + d 
+		subprocess.call(e, shell = True)
+
+		
